a2bd6353121e1542f2cb7b8e8331d290c5426c18
leemutai <mutailee361@gmail.com>
2025-04-14 22:25:24 +0300

    enhanced main banner and conditionally render navbar Added heading and CTA buttons to MainBanner component Linked banner buttons to products page Updated App layout to hide Navbar on /seller routes Applied responsive padding for consistent UI across pages

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
 
 const MainBanner = () => {
   return (
@@ -15,6 +16,35 @@ const MainBanner = () => {
         alt="banner"
         className="w-full hidden md:hidden"
       />
+
+      <div>
+        <h1>Freshness You can Trust, Savings You will Love!</h1>
+      </div>
+      <div>
+        <Link
+          to={"/products"}
+          className="group flex items-center  gap-2 px-7 md:px-9 py-3 bg-primary  hover:bg-primary-dull transition rounded text-white cursor-pointer"
+        >
+          Show Now
+          <img
+            className="md:hidden transition group-focus:translate-x-1"
+            src={assets.white_arrow_icon}
+            alt="arrow"
+          />
+        </Link>
+
+        <Link
+          to={"/products"}
+          className=" group hidden md:flex items-center  gap-2 px-7 md:px-9 py-3  cursor-pointer"
+        >
+          Explore deals
+          <img
+            className="transition group-hover:translate-x-1"
+            src={assets.black_arrow_icon}
+            alt="arrow"
+          />
+        </Link>
+      </div>
     </div>
   );
 };
