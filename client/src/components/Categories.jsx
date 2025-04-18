b8e7dc691f5b797ab5b53f1c642ca8801e1efce2
leemutai <mutailee361@gmail.com>
2025-04-18 23:09:05 +0300

    added dynamic category listing with navigation + fix JSX bug Rendered categories dynamically from assets Enabled navigation on category click Applied custom background colors and hover styles Fixed syntax error by removing extra semicolon in JSX block

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,19 +1,31 @@
 import React from "react";
-import { assets } from "../assets/assets";
+import { categories } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
 
 const Categories = () => {
+  const { navigate } = useAppContext();
   return (
     <div className="mt-16">
       <p className="text-2xl md:text-3xl font-medium">Categories</p>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 mt-6 gap-6">
-        <div className="grop cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center">
-          <img
-            className="group-hover:scale-108 transition max-w-28"
-            src={assets.box_icon}
-            alt=""
-          />
-          <p className="text-sm font-medium">Fruit</p>
-        </div>
+        {categories.map((category, index) => (
+          <div
+            key={index}
+            className="grop cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center"
+            style={{ backgroundColor: category.bgColor }}
+            onClick={() => {
+              navigate(`/products/${category.path.toLocaleLowerCase()}`);
+              scrollTo(0, 0);
+            }}
+          >
+            <img
+              className="group-hover:scale-108 transition max-w-28"
+              src={category.image}
+              alt="category.text"
+            />
+            <p className="text-sm font-medium">{category.text}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
