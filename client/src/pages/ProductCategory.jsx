93b6c292122f54db7c51f85e93a2d2765c074fbc
leemutai <mutailee361@gmail.com>
2025-04-29 11:40:00 +0300

    feat: display filtered products by category with responsive grid and fallback message

--- client/src/pages/ProductCategory.jsx
+++ client/src/pages/ProductCategory.jsx
@@ -2,6 +2,7 @@ import React, { use } from "react";
 import { useAppContext } from "../context/AppContext";
 import { useParams } from "react-router-dom";
 import { categories } from "../assets/assets";
+import ProductCard from "../components/ProductCard";
 
 const ProductCategory = () => {
   const { products } = useAppContext();
@@ -20,6 +21,19 @@ const ProductCategory = () => {
             {searchCategory.text.toUpperCase()}
           </p>
           <div className="w-16 h-0.5 bg-primary rounded-full"></div>
+        </div>
+      )}
+      {filteredProducts.length > 0 ? (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6">
+          {filteredProducts.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center h-[60vh]">
+          <p className="text-2xl font-medium text-primary">
+            No products fund in this category.
+          </p>
         </div>
       )}
     </div>
