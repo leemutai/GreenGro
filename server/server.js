697bb315d6cbf250316a929f097ffa1e560b5e67
leemutai <mutailee361@gmail.com>
2025-06-05 23:38:14 +0300

    eat: setup backend server and MongoDB connection with Express and Mongoose Initialized Express server with basic middleware setup (JSON parsing, CORS, cookies) Connected to MongoDB using Mongoose via a separate config file Created .env file to manage MongoDB URI securely Structured backend with folders for configs, models, routes, controllers, and middlewares Added a root test route to confirm server is running

--- server/server.js
+++ server/server.js
@@ -1,7 +1,21 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
+import cors from 'cors';
+import connectDB from './configs/db.js';
+import 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+await connectDB();
+
+// allow multiple origins
+const allowedOrigins = ['http://localhost:5173']
+
+// Middleware configuration
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({origin: allowedOrigins, Credentials: true}));
 
 app.get('/',(req,res) => res.send("Api is working"));
 
