faa5e4cee71e56f268e1622c6ac803be26ed8c0d
leemutai <mutailee361@gmail.com>
2025-06-23 01:05:58 +0300

    fix: add deployed frontend URL to CORS allowed origins Included https://green-gro.vercel.app in allowedOrigins array Resolved CORS/network error on Vercel deployment

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ await connectCloudinary();
 
 
 // allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173','https://green-gro.vercel.app']
 
 
 app.post('/stripe', express.raw({type: 'application/json'}),stripeWebhooks);
